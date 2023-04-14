d0ff8e8559cf8ed9c794525c6396c6057a6f9314
Dmitry Kurtaev <dmitry.kurtaev@gmail.com>
2023-04-14 19:46:53 +0300

    Add labeler bot (#1)

---
+++ label_bot/index.js
@@ -0,0 +1,58 @@
+const http = require('http');
+const { Octokit } = require("@octokit/core");
+const { createAppAuth } = require("@octokit/auth-app");
+
+const port = process.env.PORT || 80;
+
+const installationOctokit = new Octokit({
+    authStrategy: createAppAuth,
+    auth: {
+      appId: 318270,
+      privateKey: process.env.PRIVATE_KEY,
+      installationId: 36435735,
+    },
+});
+
+async function checkReadiness(pull_id) {
+    // Count number of unique approvers
+    const result = await installationOctokit.request("GET /repos/{owner}/{repo}/pulls/{pull_id}/reviews", {
+        owner: "Teddy-Hackers",
+        repo: "devtools-course-practice",
+        pull_id: pull_id,
+    });
+
+    let approvers = new Set();
+    result.data.forEach(entry => {
+        if (entry.state == 'APPROVED') {
+            approvers.add(entry.user.login);
+        }
+    });
+
+    if (approvers.size < 2)
+        return;
+
+    // Put a readiness label
+    installationOctokit.request("POST /repos/{owner}/{repo}/issues/{pull_id}/labels", {
+        owner: "Teddy-Hackers",
+        repo: "devtools-course-practice",
+        pull_id: pull_id,
+        labels: ["Ready for merge"],
+    });
+}
+
+http.createServer(function (req, res) {
+    // Get JSON data for POST request
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk.toString();
+    });
+    req.on('end', () => {
+        body = JSON.parse(body);
+
+        if (body.review.state == "approved") {
+            checkReadiness(body.pull_request.number);
+        }
+
+        res.end();
+    });
+}).listen(port);
