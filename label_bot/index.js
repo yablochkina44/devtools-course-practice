345cae2f2a088d3a3c2caeb07a6371d6af56d0eb
Dmitry Kurtaev <dmitry.kurtaev@gmail.com>
2023-04-25 15:00:18 +0300

    Put labels for lab number

--- label_bot/index.js
+++ label_bot/index.js
@@ -13,6 +13,39 @@ const installationOctokit = new Octokit({
     },
 });
 
+async function putLabNumber(pull_id) {
+    const pullInfo = await installationOctokit.request("GET /repos/{owner}/{repo}/pulls/{pull_id}", {
+        owner: "Teddy-Hackers",
+        repo: "devtools-course-practice",
+        pull_id: pull_id,
+    });
+
+    let task_id = pullInfo.data.title.match(/\d+/);
+    if (!task_id)
+        return;
+
+    task_id = parseInt(task_id[0]);
+    if (task_id < 1 || task_id > 3)
+        return;
+
+    const pullFiles = await installationOctokit.request("GET /repos/{owner}/{repo}/pulls/{pull_id}/files", {
+        owner: "Teddy-Hackers",
+        repo: "devtools-course-practice",
+        pull_id: pull_id,
+    });
+
+    if (pullFiles.data.length == 0 ||
+        pullFiles.data.length == 1 && pullFiles.data[0].filename == "lab-guide/topics.md") {
+        return;
+    }
+    installationOctokit.request("POST /repos/{owner}/{repo}/issues/{pull_id}/labels", {
+        owner: "Teddy-Hackers",
+        repo: "devtools-course-practice",
+        pull_id: pull_id,
+        labels: ["lab " + task_id],
+    });
+}
+
 async function checkReadiness(pull_id) {
     // Count number of unique approvers
     const result = await installationOctokit.request("GET /repos/{owner}/{repo}/pulls/{pull_id}/reviews", {
@@ -40,6 +73,20 @@ async function checkReadiness(pull_id) {
     });
 }
 
+// Put labels to existing pull requests
+// installationOctokit.paginate(installationOctokit.rest.pulls.list, {
+//     owner: "Teddy-Hackers",
+//     repo: "devtools-course-practice",
+//     state: "all",
+//     sort: "created",
+//     direction: "asc",
+// })
+// .then(pulls => {
+//     pulls.forEach(pull => {
+//         putLabNumber(pull.number);
+//     });
+// });
+
 http.createServer(function (req, res) {
     // Get JSON data for POST request
     let body = '';
@@ -49,6 +96,7 @@ http.createServer(function (req, res) {
     req.on('end', () => {
         try {
             body = JSON.parse(body);
+            putLabNumber(body.pull_request.number);
             if (body.review.state == "approved") {
                 checkReadiness(body.pull_request.number);
             }
