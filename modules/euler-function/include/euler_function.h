b490fc5fcf0e05fffa4d04b8ed26680675d819e8
FirstStep0 <75475364+FirstStep0@users.noreply.github.com>
2023-05-01 17:43:09 +0300

    Шокуров - Лабораторная работа #2 (#121)
    * Lab2. added files
    
    * Lab 2: fix style
    
    * Lab 2: fix style 2
    
    * Lab 2: fix style 3
    
    * Lab 2: fix style 4
    
    * Lab 2: fix style 5
    
    * lab 2: fix style 6
    
    * Lab 2: fix style 7
    
    * Lab 2: add tests for big and random value
    
    * Lab 2: fix style
    
    * Lab 2: fix style

---
+++ modules/euler-function/include/euler_function.h
@@ -0,0 +1,12 @@
+﻿  // Copyright 2023 Shokurov Daniil
+
+#ifndef MODULES_EULER_FUNCTION_INCLUDE_EULER_FUNCTION_H_
+#define MODULES_EULER_FUNCTION_INCLUDE_EULER_FUNCTION_H_
+
+#include <math.h>
+#include <string>
+// the number of natural numbers coprime with n from the segment [1, n−1]
+
+int64_t euler_function(int64_t n);
+
+#endif  // MODULES_EULER_FUNCTION_INCLUDE_EULER_FUNCTION_H_
